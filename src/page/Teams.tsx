675e2966be59e3ff7ca8f803247cbf2586fcf720
RabinThapa1998 <imrabin1998@gmail.com>
2023-03-03 21:38:55 +0545

    style(teams):add teams card

--- src/page/Teams.tsx
+++ src/page/Teams.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { icons } from '~/assets';
+
 export function Teams() {
   return (
-    <div>
-      <h1 className='text-8xl'>Helo</h1>
+    <div className=''>
+      <h1>Manage Users</h1>
+      <div className='flex'>
+        <div className='h-[88px] w-[304px] bg-brand-primary rounded-[5px] px-5 flex flex-row items-center justify-between'>
+          <div className='flex flex-col text-white'>
+            <p>Teams</p>
+            <p>23</p>
+          </div>
+          <div className='h-[42px] w-[42px] flex flex-row items-center justify-center rounded-[5px] bg-[#FFFFFF33]'>
+            {<icons.teams />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
