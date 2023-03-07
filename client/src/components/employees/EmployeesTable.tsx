166759cf76618056edb4e27d7fd40d79b887e455
RabinThapa1998 <imrabin1998@gmail.com>
2023-03-07 11:17:47 +0545

    feat:add employee get api

--- client/src/components/employees/EmployeesTable.tsx
+++ client/src/components/employees/EmployeesTable.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
 import { Space, Table, Tag } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { IEmployeeSummary } from '~/types';
+import { IEmployeeResponse, IEmployeeSummary } from '~/types';
 import { employeeSelector, useAppSelector } from '~/global-states';
 import { Icons } from '~/assets';
+import { useQuery } from '@tanstack/react-query';
+import { API_BASE_URL } from '~/config';
 
 const columns: ColumnsType<IEmployeeSummary> = [
   {
@@ -51,7 +53,7 @@ const columns: ColumnsType<IEmployeeSummary> = [
   {
     title: 'Billable Hours',
     dataIndex: 'billable_hours',
-    key: 'billable_hours',
+    key: 'billable_hrs',
 
     width: 150,
   },
@@ -78,25 +80,34 @@ const columns: ColumnsType<IEmployeeSummary> = [
 ];
 
 export function EmployeesTable() {
-  const employeeList = useAppSelector(employeeSelector);
+  // const employeeList = useAppSelector(employeeSelector);
+
+  const { data: employeeList, isLoading } = useQuery(['employeeTable'], () =>
+    fetch(new URL('employee', API_BASE_URL)).then(
+      (res) => res.json() as Promise<IEmployeeResponse>,
+    ),
+  );
+  console.log('🚀 ~ file: EmployeesTable.tsx:88 ~ EmployeesTable ~ data:', employeeList);
 
   const employeeFormattedData = useMemo(() => {
-    const temp = employeeList.map((item, idx) => {
+    const temp = employeeList?.data.map((item, idx) => {
       return {
-        id: idx,
+        id: idx + 1,
         full_name: item.name + ' ' + item.middle_name + ' ' + item.surname,
-        current_team: item.team,
+        current_team: item.team.length ? item.team.map((i) => i) : 'Available',
         mobile_number: item.phone_number,
         email_address: item.email,
         designation: item.job_position,
-        billable_hours: item.billable_hrs,
+        billable_hrs: `${item.billable_hrs} hours/week`,
       };
     });
     return temp;
   }, [employeeList]);
+
   return (
     <Table
       columns={columns}
+      loading={isLoading}
       dataSource={employeeFormattedData}
       pagination={{ position: ['bottomRight'] }}
       className='table'
--- client/src/types/shared.ts
+++ client/src/types/shared.ts
@@ -1,11 +1,11 @@
 export interface IEmployeeSummary {
   id: number;
   full_name: string;
-  current_team: string;
+  current_team: string | string[];
   mobile_number: string;
   email_address: string;
   designation: string;
-  billable_hours: string;
+  billable_hrs: string;
 }
 export interface IEmployeeData {
   name: string;
@@ -28,3 +28,34 @@ export type IBreadCrumbs = {
   title: string;
   link: string;
 }[];
+
+export interface IEmployeeResponse {
+  data: ISingleEmployeeResponse[];
+}
+
+export interface ISingleEmployeeResponse {
+  team: string[];
+  name: string;
+  middle_name: string;
+  surname: string;
+  dob: string;
+  address: string;
+  phone_number: string;
+  email: string;
+  gender: Gender;
+  starts_at: string;
+  ends_at: string;
+  job_position: string;
+  is_billable: boolean;
+  billable_hrs: number;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  id: string;
+}
+
+export enum Gender {
+  Female = 'female',
+  Male = 'male',
+  Other = 'other',
+}
