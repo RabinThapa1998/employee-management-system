918b28993b2c195660fae378ddbec42b1e640eb7
RabinThapa1998 <imrabin1998@gmail.com>
2023-03-08 23:58:48 +0545

    fix(team-update):update operation

--- server/src/controllers/v1/dashboard/team/update.ts
+++ server/src/controllers/v1/dashboard/team/update.ts
@@ -9,20 +9,17 @@ export const updateTeamHandler = async (req: Request, res: Response) => {
 
     const team = await Team.findById(id);
     if (!team) throw new BadRequestError("Team Not Found");
+    const oldTeamList = team.members;
+
     //check if any of the members are from other teams or empty
-    const memberAlreadyOnOtherTeamOrUnassigned = await Employee.find({
+    const membersAlreadyAssigned = await Employee.find({
       _id: { $in: members },
       team: { $ne: team.id },
     });
 
-    const memberAlreadyOnOtherTeam =
-      memberAlreadyOnOtherTeamOrUnassigned.filter(
-        (member) => member.team.length
-      );
-    if (memberAlreadyOnOtherTeam.length)
+    if (membersAlreadyAssigned.filter((member) => member.team.length).length)
       throw new BadRequestError("Member already on other team");
 
-    //update team
     team.name = name || team.name;
     team.password = password || team.password;
     team.members = members || team.members;
@@ -38,17 +35,19 @@ export const updateTeamHandler = async (req: Request, res: Response) => {
         await employee.save();
       }
     }
-    //remove team from employee if not in members
-    const employeeNotInMembers = team.members.filter(
-      (old) => !members.includes(old)
-    );
+    // remove team from employee if not in members
+
+    const employeeNotInMembers = [
+      ...new Set([...oldTeamList.map((list) => list.toString()), ...members]),
+    ].filter((member) => !members.includes(member));
     console.log(
-      "🚀 ~ file: update.ts:45 ~ updateTeamHandler ~ employeeNotInMembers:",
-      employeeNotInMembers
+      "🚀 ~ file: update.ts:43 ~ updateTeamHandler ~ employeeNotInMembers:",
+      employeeNotInMembers,
+      [...new Set([...oldTeamList.map((list) => list.toString()), ...members])]
     );
     if (employeeNotInMembers.length) {
-      for (let i = 0; i < team.members.length; i++) {
-        const employee = await Employee.findById(team.members[i]);
+      for (let i = 0; i < employeeNotInMembers.length; i++) {
+        const employee = await Employee.findById(employeeNotInMembers[i]);
         if (!employee) throw new BadRequestError("Employee not found");
         employee.team = [];
         await employee.save();
@@ -64,3 +63,50 @@ export const updateTeamHandler = async (req: Request, res: Response) => {
     );
   }
 };
+
+//  //check if any of the members are from other teams or empty
+//  const memberAlreadyOnOtherTeamOrUnassigned = await Employee.find({
+//   _id: { $in: members },
+//   team: { $ne: team.id },
+// });
+
+// const memberAlreadyOnOtherTeam =
+//   memberAlreadyOnOtherTeamOrUnassigned.filter(
+//     (member) => member.team.length
+//   );
+// if (memberAlreadyOnOtherTeam.length)
+//   throw new BadRequestError("Member already on other team");
+// const oldTeamList = team.members;
+// //update team
+// team.name = name || team.name;
+// team.password = password || team.password;
+// team.members = members || team.members;
+// team.billable_hrs = billable_hrs || team.billable_hrs;
+// await team.save();
+
+// //update employee with new team
+// if (members.length) {
+//   for (let i = 0; i < members.length; i++) {
+//     const employee = await Employee.findById(members[i]);
+//     if (!employee) throw new BadRequestError("Employee not found");
+//     employee.team = [team.id];
+//     await employee.save();
+//   }
+// }
+// //remove team from employee if not in members
+
+// const employeeNotInMembers = oldTeamList.filter(
+//   (member) => !members.includes(member)
+// );
+// console.log(
+//   "🚀 ~ file: update.ts:45 ~ updateTeamHandler ~ employeeNotInMembers:",
+//   employeeNotInMembers
+// );
+// if (employeeNotInMembers.length) {
+//   for (let i = 0; i < employeeNotInMembers.length; i++) {
+//     const employee = await Employee.findById(employeeNotInMembers[i]);
+//     if (!employee) throw new BadRequestError("Employee not found");
+//     employee.team = [];
+//     await employee.save();
+//   }
+// }
