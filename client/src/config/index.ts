4dd6ed37832b8712aa81216b4597ee4d97a252fd
RabinThapa1998 <imrabin1998@gmail.com>
2023-08-21 10:06:49 +0545

    update config

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,4 +2,4 @@ const dev = import.meta.env.DEV;
 
 export const API_BASE_URL = dev
   ? 'http://localhost:8000/api/v1/dashboard/'
-  : process.env.NEXT_PUBLIC_API_BASE_URL!;
+  : process.env.VITE_API_BASE_URL!;
